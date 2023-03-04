864e647fb674ba2a20de589cf293f9332ec352d7
Alessio <alessio.petrotta1@gmail.com>
2023-03-04 12:48:32 +0100

    fixed insert.php more flexible

--- sql/insert.php
+++ sql/insert.php
@@ -1,11 +1,11 @@
 <?php 
     require_once "connect.php";
     require_once "library.php";
-    $columns_values = fromPOSTtoString($_POST);
+    $columns_values = fromPOSTtoStringS($_POST);
     $columns = $columns_values[0];
     $values = $columns_values[1];
-    $values = $_POST['name'].','.$_POST['surname'].','.$_POST['age'];
     $values = addSingleQuote($values);
+    myPrintArray($columns_values);
     statement($pdo,$columns,$values);
     
     echo "sending data";
