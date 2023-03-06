be143ae1aaafb49922c4a68d9585b9e33e3f522f
PetroAless <alessio.petrotta1@gmail.com>
2023-03-06 13:42:15 +0100

    added html part of the whole db now

--- sql/insert.php
+++ sql/insert.php
@@ -2,6 +2,8 @@
     require_once "connect.php";
     require_once "library.php";
     unset($_POST['submit']);
+    $_POST['gender']=$_POST['gender'][0];
+
     $columns_values = fromPOSTtoStringS($_POST);
     $columns = $columns_values[0];
     $values = $columns_values[1];
