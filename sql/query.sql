29d4fae753929a99ec3aa547879c9fffcdf7c763
Alessio <alessio.petrotta1@gmail.com>
2023-03-06 18:55:27 +0100

    fix+ notes

--- sql/query.sql
+++ sql/query.sql
@@ -1,11 +1,1 @@
-CREATE TABLE PPL(
-    ID int UNIQUE NOT NULL AUTO_INCREMENT,
-    firstName varchar(255) DEFAULT "John",
-    lastName varchar(255) DEFAULT "Doe",
-    dateOfBirth DATE,
-    gender ENUM('M','F','O'),
-    email varchar(255),
-    country varchar(255),
-    city varchar(255),
-    PRIMARY KEY(ID)
-);
+SELECT * FROM PPL ORDER BY dateOfBirth;
